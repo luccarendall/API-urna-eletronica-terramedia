9aa10218ad2152a5ef6b036d690e018cb68ef84f
LuccaRendall <luccarendall2@gmail.com>
2023-09-13 12:41:19 -0300

    feat: Rota para buscar candidato pelo id - concluída

--- routes/candidates-routes.js
+++ routes/candidates-routes.js
@@ -8,6 +8,14 @@ router.get('/candidatos', async (req, res) => {
   res.json(results)
 });
 
+// rota para listar 1 candidato especifico com base no id
+router.get('/candidatos/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const results = await db.getProductById(id)
+  res.json(results)
+});
+
+
 // rota de cadastro de 1 novo candidato
 router.post('/candidato', async (req, res) => {
   const candidato = req.body;
